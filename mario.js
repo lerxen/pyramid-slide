e1ceab1c60bce61c8a15e625a58232bb5eb53e73
Lera <lerxen13@yandex.ru>
2020-07-13 14:25:51 +0300

    change code using jQuery

--- mario.js
+++ mario.js
@@ -2,43 +2,35 @@ printPyramid(11,'#');
 
 function printPyramid(height,string) {
   let str = string+string;
-  let pyr = document.getElementById('pyramid');
-  pyr.textContent = "";
+  let pyr = $('#pyramid')//.eq(0);
+  //let pyr = document.getElementById('pyramid');
+  pyr.text(""); //pyr.textContent = "";
   for (let i = 1; i < height; i++){
     str += "\u00A0";
   }
   str = str.split('').reverse().join('');
-  let p = document.createElement('p');
-  p.innerHTML = str;
+  let p = $('<p>'); //let p = document.createElement('p');
+  p.text(str); //p.innerHTML = str;
   pyr.append(p);
-  //console.log(str);
   for (let i = 0; i < height-1; i++){
     str = str.slice(1);
     str += string;
-    let elm = document.createElement('p');
-    elm.innerHTML = str;
+    let elm = $('<p>'); //let elm = document.createElement('p');
+    elm.text(str); //elm.innerHTML = str;
     pyr.append(elm);
-    //console.log(str);
   }
 
 }
-let symbol = document.getElementById('symbol');
-let range = document.getElementById('height');
-let rangeValue = document.getElementById('inputValue');
-rangeValue.innerHTML = range.value;
+let symbol = $('#symbol').eq(0); //let symbol = document.getElementById('symbol');
+let range = $('#height'); //let range = document.getElementById('height');
+let rangeValue = $('#inputValue'); //let rangeValue = document.getElementById('inputValue');
+rangeValue.text(range.val()); //rangeValue.innerHTML = range.value;
 
 function showValue() {
-  rangeValue.innerHTML = range.value;
-  printPyramid(range.value,symbol.value);
+  rangeValue.text(range.val()); //rangeValue.innerHTML = range.value;
+  printPyramid(range.val(), symbol.val()); //printPyramid(range.value,symbol.value);
 }
 
 function changePyr() {
-  printPyramid(range.value, symbol.value);
+  printPyramid(range.val(),symbol.val()); //printPyramid(range.value, symbol.value);
 }
-
-/*function check() {
-  let para = document.getElementById('check');
-  para.innerHTML = range.value;
-}
-
-range.addEventListener('click', check); */
